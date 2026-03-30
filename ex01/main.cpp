a3dcd45f46fb94808569ffcdc1ea3d32385d3c69
Sacha Kock <skock@c2r12p2.42nice.fr>
2026-03-30 14:57:07 +0200

    ex01 added

---
+++ ex01/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skock <skock@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 13:28:24 by sacha             #+#    #+#             */
+/*   Updated: 2026/03/30 13:11:10 by skock            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cerr << "Usage: " << av[0] << " <input_file>" << std::endl;
+		return 1;
+	}
+	BitcoinExchange exchange;
+	exchange.loadExchangeRatesCsv("data.csv");
+	exchange.loadInputFile(av[1]);
+	exchange.printConvertedOutput();
+	return 0;
+}
